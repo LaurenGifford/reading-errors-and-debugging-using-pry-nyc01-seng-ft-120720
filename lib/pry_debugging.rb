f6697aa9569ef4fc142f2df9cc54fefe6a352480
Lauren Gifford <lauren.gifford.17@gmail.com>
2020-11-21 00:51:18 +0000

    Automatically backed up by Learn

--- lib/pry_debugging.rb
+++ lib/pry_debugging.rb
@@ -1,5 +1,14 @@
+<<<<<<< HEAD
 
 def plus_two(num)
 	sum = num + 2
 	sum
+=======
+require 'pry'
+
+def plus_two(num)
+	num + 2
+	num
+	binding.pry
+>>>>>>> 70fabbe856068e901efd7c62a0e109e7d31acc7b
 end
